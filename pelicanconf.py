406f758f380fa5d8b7e4089c5f3e1f044bc4e81e
Thomas Van Doren <thomas.vandoren@gmail.com>
2014-07-13 18:32:32 -0700

    update conf

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,30 +2,48 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
+import os.path
+
+
 AUTHOR = u'Thomas Van Doren'
 SITENAME = u"Thomas Van Doren's Blog"
-SITEURL = ''
+SITEURL = 'http://thomasvandoren.com/blog'
 
 TIMEZONE = 'US/Pacific'
 
 DEFAULT_LANG = u'en'
 
 # Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
-TRANSLATION_FEED_ATOM = None
+FEED_DOMAIN = SITEURL
+FEED_ATOM = 'feeds/atom.xml'
+FEED_RSS = 'feeds/rss.xml'
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
+CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
 
 # Blogroll
-LINKS =  (('Pelican', 'http://getpelican.com/'),
-          ('Python.org', 'http://python.org/'),
-          ('Jinja2', 'http://jinja.pocoo.org/'),
-          ('You can modify those links in your config file', '#'),)
+LINKS =  (
+    ('Chapel', 'http://chapel.cray.com/'),
+    ('Cray Blog', 'http://blog.cray.com/'),
+    ('Pelican', 'http://getpelican.com/'),
+    ('Python.org', 'http://python.org/'),
+    ('Jinja2', 'http://jinja.pocoo.org/'),
+)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (
+    ('GitHub', 'https://github.com/thomasvandoren'),
+    ('LinkedIn', 'https://www.linkedin.com/in/thomasvandoren'),
+    ('Facebook', 'https://facebook.com/thomasvandoren'),
+)
 
 DEFAULT_PAGINATION = False
+
+STATIC_PATHS = [
+    'images',
+    'presentations',
+]
+
+THEME = os.path.expanduser("~/src/pelican-themes/tuxlite_tbs")
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
